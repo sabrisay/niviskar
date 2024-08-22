2f6514790b72722471cdb3633a8a8714283bff77
Sabri Sayilgan <38506999+sabrisay@users.noreply.github.com>
2024-08-22 09:36:08 -0700

    Update snapshots-amis.sh

--- Bash/snapshots-amis.sh
+++ Bash/snapshots-amis.sh
@@ -7,14 +7,8 @@ AMI_ID="ami-xxxxxxxxxxxxxxxxx"
 aws ec2 deregister-image --image-id $AMI_ID
 echo "Deregistered AMI: $AMI_ID"
 
-# Get the Block Device Mappings for the AMI to identify snapshots
-SNAPSHOT_IDS=$(aws ec2 describe-images --image-ids $AMI_ID --query "Images[*].BlockDeviceMappings[*].Ebs.SnapshotId" --output text)
-
-# Check if any snapshots were found
-if [ -z "$SNAPSHOT_IDS" ]; then
-    echo "No snapshots found for AMI: $AMI_ID"
-    exit 0
-fi
+# Get the list of snapshots associated with the AMI
+SNAPSHOT_IDS=$(aws ec2 describe-snapshots --filters "Name=description,Values=*$AMI_ID*" --query "Snapshots[*].SnapshotId" --output text)
 
 # Delete each snapshot
 for SNAPSHOT_ID in $SNAPSHOT_IDS; do
